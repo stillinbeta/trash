0e8de321094eeca44c057be1c246705a69fe7c8a
stillinbeta <web@stillinbeta.com>
2011-05-15 01:01:45 -0500

    Basic exec functionality

--- trash.c
+++ trash.c
@@ -2,6 +2,8 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <errno.h>
+
 
 int main() {
     char buf[256];
@@ -12,12 +14,13 @@ int main() {
     int n;
 
     fgets(buf,sizeof(buf)-1,stdin);
+    buf[sizeof(buf) -1] = '\0';
     len = strlen(buf);
     buf[len - 1]='\0'; //cut out the final newline
     
     count = 0;
     words[0] = buf;
-    for(i = 0; i < len; i++) {
+    for(i = 0; i < len && count < sizeof(words)-2; i++) {
         if(buf[i] == ' ') {
             buf[i] = '\0';
             if(i > 0 && buf[i-1] != 0) { //skip over double spaces
@@ -26,10 +29,13 @@ int main() {
             words[count] = &buf[i+1];
         }
     }
-
+    words[count+1] = NULL; //NULL terminate for execvp
+    
     n = fork();
     if (n == 0) { //child
-        printf("child");
+        errno = 0;
+        execvp(words[0],words);
+        perror("");
     }
     else {
         wait(NULL);
